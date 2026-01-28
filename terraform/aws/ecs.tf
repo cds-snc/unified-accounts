0c575a3f95ea2817ab263a36b261aa0caeffa78b
Pat Heard <patrick.heard@cds-snc.ca>
2026-01-28 09:02:20 -0500

    feat: connect to API through local IP address
    Add service discovery to the IDP ECS tasks so the API can make requests
    to the IDP through its local private subnet IP address rather than needing
    to request out through the internet.

--- terraform/aws/ecs.tf
+++ terraform/aws/ecs.tf
@@ -67,7 +67,7 @@ locals {
   login_container_env = [
     {
       "name"  = "ZITADEL_API_URL",
-      "value" = "https://${var.domain}"
+      "value" = "http://idp.${aws_service_discovery_private_dns_namespace.idp.name}:8080"
     },
     {
       "name"  = "NEXT_PUBLIC_BASE_PATH",
@@ -146,8 +146,10 @@ module "idp_ecs" {
       container_port   = 8080
     }
   ]
-  subnet_ids         = module.idp_vpc.private_subnet_ids
-  security_group_ids = [aws_security_group.idp_ecs.id]
+  subnet_ids                     = module.idp_vpc.private_subnet_ids
+  security_group_ids             = [aws_security_group.idp_ecs.id]
+  service_discovery_enabled      = true
+  service_discovery_namespace_id = aws_service_discovery_private_dns_namespace.idp.id
 
   billing_tag_value = var.billing_tag_value
 
@@ -212,9 +214,11 @@ module "login_ecs" {
     }
   }]
 
-  lb_target_group_arn = aws_lb_target_group.idp_login.arn
-  subnet_ids          = module.idp_vpc.private_subnet_ids
-  security_group_ids  = [aws_security_group.idp_login_ecs.id]
+  lb_target_group_arn            = aws_lb_target_group.idp_login.arn
+  subnet_ids                     = module.idp_vpc.private_subnet_ids
+  security_group_ids             = [aws_security_group.idp_login_ecs.id]
+  service_discovery_enabled      = true
+  service_discovery_namespace_id = aws_service_discovery_private_dns_namespace.idp.id
 
   billing_tag_value = var.billing_tag_value
 
