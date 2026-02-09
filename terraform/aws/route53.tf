15edfc82a1b3a793aff403e19f674a4f3262df98
Pat Heard <patrick.heard@cds-snc.ca>
2026-02-09 13:44:39 -0500

    feat: add DNS records to improve email security (#24)
    The records being added are as follows:
    
    - `SPF`- Authorizes Amazon SES to send emails on behalf of this domain 
    and blocks all other sources.
    - `DKIM` - Adds cryptographic signatures to outgoing emails so recipients 
    can verify they actually came from the IdP's domain.
    - `DMARC` - Instructs receiving mail servers to reject emails that fail 
    SPF/DKIM checks and sends failure reports to the CDS security team.

--- terraform/aws/route53.tf
+++ terraform/aws/route53.tf
@@ -14,3 +14,44 @@ resource "aws_route53_record" "idp_A" {
     evaluate_target_health = false
   }
 }
+
+# SES verification
+resource "aws_route53_record" "idp_verification_TXT" {
+  zone_id = aws_route53_zone.idp.zone_id
+  name    = "_amazonses.${aws_ses_domain_identity.idp.id}"
+  type    = "TXT"
+  ttl     = "600"
+  records = [aws_ses_domain_identity.idp.verification_token]
+}
+
+# Email sending
+resource "aws_route53_record" "idp_spf_TXT" {
+  zone_id = aws_route53_zone.idp.zone_id
+  name    = var.domain
+  type    = "TXT"
+  ttl     = "300"
+  records = [
+    "v=spf1 include:amazonses.com -all"
+  ]
+}
+
+resource "aws_route53_record" "idp_dkim_CNAME" {
+  count   = 3
+  zone_id = aws_route53_zone.idp.zone_id
+  name    = "${element(aws_ses_domain_dkim.idp.dkim_tokens, count.index)}._domainkey.${var.domain}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [
+    "${element(aws_ses_domain_dkim.idp.dkim_tokens, count.index)}.dkim.amazonses.com",
+  ]
+}
+
+resource "aws_route53_record" "idp_dmarc_TXT" {
+  zone_id = aws_route53_zone.idp.zone_id
+  name    = "_dmarc.${var.domain}"
+  type    = "TXT"
+  ttl     = "300"
+  records = [
+    "v=DMARC1; p=reject; sp=reject; pct=100; rua=mailto:CDS.SECURITY-SECURITE.SNC@servicecanada.gc.ca"
+  ]
+}
